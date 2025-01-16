52add3f5720e0b56c49ba4afefb6ed560551b522
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2025-01-17 00:02:38 +0800

    Fix imports

--- app/_components/WatchListButton/action.js
+++ app/_components/WatchListButton/action.js
@@ -1,8 +1,8 @@
 'use server';
 
-import clientPromise from '@/src/lib/mongodb';
+import clientPromise from '@/app/lib/mongodb';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/src/lib/authOptions';
+import { authOptions } from '@/app/lib/authOptions';
 
 export const handleAddToList = async (id, title) => {
   try {
